09766e392299632f000d0edaacd1a7bca99b215a
nikhilnambi <96744136+nikhilnambi@users.noreply.github.com>
2022-04-25 18:37:20 +0530

    Update signupdata.js

--- src/model/signupdata.js
+++ src/model/signupdata.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose"); // Accessing the mongoose package
 
- mongoose.connect("mongodb://localhost:27017/Library"); // connecting and creating database
+//Database connection
+//mongoose.connect('mongodb://localhost:27017/Library');
+
+mongoose.connect("mongodb+srv://usernk:usernk@ictak.svswr.mongodb.net/Library?retryWrites=true&w=majority");
 
 //Schema definition
 const Schema = mongoose.Schema;
